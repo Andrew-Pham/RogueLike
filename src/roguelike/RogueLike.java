4438dc0178c4955c03e7f88ce0fc9086d8dbdc64
Andrew Pham <andrew.dinh.pham@gmail.com>
2014-11-22 13:29:24 -0500

    Initial gameplay prototype
    * Initialize console
    * Add moving character

--- src/roguelike/RogueLike.java
+++ src/roguelike/RogueLike.java
@@ -5,18 +5,81 @@
  */
 package roguelike;
 
+import com.googlecode.blacken.colors.ColorNames;
+import com.googlecode.blacken.colors.ColorPalette;
+import com.googlecode.blacken.swing.SwingTerminal;
+import com.googlecode.blacken.terminal.BlackenKeys;
+import com.googlecode.blacken.terminal.CursesLikeAPI;
+import com.googlecode.blacken.terminal.FontNotFoundException;
+import com.googlecode.blacken.terminal.TerminalInterface;
+import com.googlecode.blacken.terminal.TerminalCellLike;
+import com.googlecode.blacken.terminal.TerminalCell;
+
 /**
  *
  * @author Andrew
  */
 public class RogueLike {
 
-    /**
-     * @param args the command line arguments
-     */
+    private ColorPalette palette;
+    private CursesLikeAPI term = null;
+    
+    public void init() {
+        
+        TerminalInterface swing = new SwingTerminal();
+        term = new CursesLikeAPI(swing);
+        term.init("Simple", 25, 80);
+        
+        palette = new ColorPalette();
+        palette.addAll(ColorNames.XTERM_256_COLORS, false);
+        palette.putMapping(ColorNames.SVG_COLORS);
+         
+        term.setPalette(palette);
+        
+        term.setCurBackground("Black");
+        term.setCurForeground("White");
+        
+        term.puts("@");
+        
+        int x = 0, y= 0;
+        TerminalCellLike space = new TerminalCell(" ");
+        TerminalCellLike player = new TerminalCell("@");
+        
+        while (true) {
+            int ch = term.getch();
+            
+            if (ch == 115) {
+                term.set(y, x, space);
+                y++;
+                term.set(y, x, player);
+            }
+            
+            if (ch == 119) {
+                term.set(y, x, space);
+                y--;
+                term.set(y, x, player);
+            }
+            
+            if (ch == 97) {
+                term.set(y, x, space);
+                x--;
+                term.set(y, x, player);
+            }
+            
+            if (ch == 100) {
+                term.set(y, x, space);
+                x++;
+                term.set(y, x, player);
+            }
+            System.out.println("character was: " + ch);
+            term.refresh();
+        }
+
+    }   
+ 
     public static void main(String[] args) {
         
-        System.out.println("hello world");
+       RogueLike rl = new RogueLike();
+       rl.init(); 
     }
-    
 }
